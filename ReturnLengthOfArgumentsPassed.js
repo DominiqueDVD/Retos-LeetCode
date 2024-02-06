fc8f730d7830293ff3848f8f237a40d9dfe9f094
Dominique <dominique.del1801@alumnos.ubiobio.cl>
2024-02-05 23:50:25 -0300

    Corrección returnLength

--- ReturnLengthOfArgumentsPassed.js
+++ ReturnLengthOfArgumentsPassed.js
@@ -4,11 +4,12 @@
  * @param {...(null|boolean|number|string|Array|Object)} args
  * @return {number}
  */
- var argumentsLength = function(...args) {
+var argumentsLength = function(...args) {
     return args.length;
 };
 
-const ejemplo1 = argumentsLength[5];
+const ejemplo1 = argumentsLength(5);
 console.log(ejemplo1);
 
-const ejemplo2 = argumen
+const ejemplo2 = argumentsLength({}, null, "3");
+console.log(ejemplo2);
